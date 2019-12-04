859e42b9d6380e1a348b5fc27d1db1b1a115d16a
Egon Elbre <egonelbre@gmail.com>
2019-12-04 11:20:29 +0200

    2019.02: reorganize code

--- 2019/day02/main.go
+++ 2019/day02/main.go
@@ -58,6 +58,10 @@ type Computer struct {
 
 type Code []int64
 
+func (code Code) Clone() Code {
+	return append(Code{}, code...)
+}
+
 type OpCode int64
 
 const (
@@ -172,11 +176,15 @@ func DecodeInstr(code Code) (instr Instr, advance int64, err error) {
 }
 
 func main() {
+	AlarmProgram(Input)
+}
+
+func AlarmProgram(input Code) {
 	cpu := Computer{
 		Halted:         false,
 		ProgramCounter: 0,
 
-		Code: Input,
+		Code: input.Clone(),
 	}
 
 	// To do this, before running the program, replace position 1 with the value 12 and replace position 2 with the value 2.
@@ -190,7 +198,8 @@ func main() {
 			panic(err)
 		}
 	}
-	fmt.Println(cpu.Code[0])
+
+	fmt.Println("Part 1", cpu.Code[0])
 }
 
 var Input = Code{1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 13, 1, 19, 1, 6, 19, 23, 2, 6, 23, 27, 1, 5, 27, 31, 2, 31, 9, 35, 1, 35, 5, 39, 1, 39, 5, 43, 1, 43, 10, 47, 2, 6, 47, 51, 1, 51, 5, 55, 2, 55, 6, 59, 1, 5, 59, 63, 2, 63, 6, 67, 1, 5, 67, 71, 1, 71, 6, 75, 2, 75, 10, 79, 1, 79, 5, 83, 2, 83, 6, 87, 1, 87, 5, 91, 2, 9, 91, 95, 1, 95, 6, 99, 2, 9, 99, 103, 2, 9, 103, 107, 1, 5, 107, 111, 1, 111, 5, 115, 1, 115, 13, 119, 1, 13, 119, 123, 2, 6, 123, 127, 1, 5, 127, 131, 1, 9, 131, 135, 1, 135, 9, 139, 2, 139, 6, 143, 1, 143, 5, 147, 2, 147, 6, 151, 1, 5, 151, 155, 2, 6, 155, 159, 1, 159, 2, 163, 1, 9, 163, 0, 99, 2, 0, 14, 0}
