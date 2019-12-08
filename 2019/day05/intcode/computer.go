45cdaf96fe1053e8a5c8d89e0560c68d4d260d54
Egon Elbre <egonelbre@gmail.com>
2019-12-08 15:33:54 +0200

    2019.05: split large switches

--- 2019/day05/intcode/computer.go
+++ 2019/day05/intcode/computer.go
@@ -27,7 +27,23 @@ func (code Code) Adjust(noun, verb int64) Code {
 	return clone
 }
 
+type Instr interface {
+	Exec(cpu *Computer) error
+	Decode(code Code) (Instr, int64, error)
+}
+
 type OpCode int64
+
+var Ops = map[OpCode]Instr{}
+
+func RegisterOp(instr Instr, code OpCode) {
+	_, exists := Ops[code]
+	if exists {
+		panic(fmt.Sprintf("code %d already registered", code))
+	}
+
+	Ops[code] = instr
+}
 
 const (
 	OpAdd      = OpCode(1)
@@ -41,108 +57,11 @@ const (
 
 	OpLessThan = OpCode(7)
 	OpEquals   = OpCode(8)
-
-	OpHalt = OpCode(99)
 )
 
 type Param struct {
 	Immediate bool
 	Value     Address
-}
-
-type (
-	Instr interface {
-		Exec(cpu *Computer) error
-	}
-
-	// [Store] := [A] + [B]
-	Add struct {
-		A, B  Param
-		Store Param
-	}
-
-	// [Store] := [A] * [B]
-	Multiply struct {
-		A, B  Param
-		Store Param
-	}
-
-	// [Store] := <-input
-	Input struct {
-		Store Param
-	}
-
-	// output <- [Load]
-	Output struct {
-		Load Param
-	}
-
-	// if [Check] != 0 then cpu.InstructionPointer := [Store]
-	JumpIfTrue struct {
-		Check  Param
-		Target Param
-	}
-	// if [Check] == 0 then cpu.InstructionPointer := [Store]
-	JumpIfFalse struct {
-		Check  Param
-		Target Param
-	}
-
-	// [Store] := [A] < [B] ? 1 : 0
-	LessThan struct {
-		A, B  Param
-		Store Param
-	}
-	// [Store] := [A] == [B] ? 1 : 0
-	Equals struct {
-		A, B  Param
-		Store Param
-	}
-
-	// cpu.Halted := true
-	Halt struct{}
-)
-
-func (op Add) Exec(cpu *Computer) error {
-	a, aerr := cpu.ValueOf(op.A)
-	b, berr := cpu.ValueOf(op.B)
-	if aerr != nil || berr != nil {
-		return fmt.Errorf("invalid arguments %+v: %v, %v", op, aerr, berr)
-	}
-
-	return cpu.Store(op.Store, a+b)
-}
-
-func (op Multiply) Exec(cpu *Computer) error {
-	a, aerr := cpu.ValueOf(op.A)
-	b, berr := cpu.ValueOf(op.B)
-	if aerr != nil || berr != nil {
-		return fmt.Errorf("invalid arguments %+v: %v, %v", op, aerr, berr)
-	}
-
-	return cpu.Store(op.Store, a*b)
-}
-
-func (op Input) Exec(cpu *Computer) error {
-	v, err := cpu.ReadInput()
-	if err != nil {
-		return err
-	}
-	return cpu.Store(op.Store, v)
-}
-
-func (op Output) Exec(cpu *Computer) error {
-	a, aerr := cpu.ValueOf(op.Load)
-	if aerr != nil {
-		return fmt.Errorf("invalid arguments %+v: %v", op, aerr)
-	}
-
-	return cpu.WriteOutput(a)
-}
-
-func (op Halt) Exec(cpu *Computer) error {
-	cpu.Halted = true
-	return nil
 }
 
 func (cpu *Computer) ValueOf(p Param) (int64, error) {
@@ -188,7 +107,7 @@ func (cpu *Computer) Step() error {
 		return fmt.Errorf("program counter overrun")
 	}
 
-	instr, advance, err := DecodeInstr(cpu.Code[cpu.InstructionPointer:])
+	instr, advance, err := Decode(cpu.Code[cpu.InstructionPointer:])
 	if err != nil {
 		cpu.Halted = true
 		return err
@@ -230,11 +149,21 @@ func (cpu *Computer) Run() error {
 	return nil
 }
 
-func DecodeInstr(code Code) (instr Instr, advance int64, err error) {
-	if len(code) == 0 {
-		return Halt{}, 0, fmt.Errorf("code missing")
+func Decode(code Code) (instr Instr, advance int64, err error) {
+	opcode, _, err := Decode0(code)
+	if err != nil {
+		return Halt{}, 0, fmt.Errorf("failed to decode opcode: %w", err)
 	}
 
+	decoder, ok := Ops[opcode]
+	if !ok {
+		return Halt{}, 0, fmt.Errorf("unknown opcode %v", opcode)
+	}
+
+	return decoder.Decode(code)
+}
+
+func ParseInstructionCode(code int64) (op OpCode, imm1, imm2, imm3 bool) {
 	// ABCDE
 	//  1002
 	//
@@ -243,97 +172,56 @@ func DecodeInstr(code Code) (instr Instr, advance int64, err error) {
 	//  B - mode of 2nd param
 	//  A - mode of 3rd param
 
-	full := code[0]
-	opcode := OpCode(full % 100)
+	return OpCode(code % 100),
+		1 == (code/100)%10,
+		1 == (code/1000)%10,
+		1 == (code/10000)%10
+}
 
-	imm1 := 1 == (full/100)%10
-	imm2 := 1 == (full/1000)%10
-	imm3 := 1 == (full/10000)%10
-
-	switch opcode {
-	case OpAdd:
-		if len(code) < 4 {
-			return Halt{}, 0, fmt.Errorf("add requires 3 arguments")
-		}
-		return Add{
-			A:     Param{imm1, code[1]},
-			B:     Param{imm2, code[2]},
-			Store: Param{imm3, code[3]},
-		}, 4, nil
-
-	case OpMultiply:
-		if len(code) < 4 {
-			return Halt{}, 0, fmt.Errorf("multiply requires 3 arguments")
-		}
-		return Multiply{
-			A:     Param{imm1, code[1]},
-			B:     Param{imm2, code[2]},
-			Store: Param{imm3, code[3]},
-		}, 4, nil
-
-	case OpInput:
-		if len(code) < 2 {
-			return Halt{}, 0, fmt.Errorf("input requires 1 arguments")
-		}
-		return Input{
-			Store: Param{imm1, code[1]},
-		}, 2, nil
-
-	case OpOutput:
-		if len(code) < 2 {
-			return Halt{}, 0, fmt.Errorf("output requires 1 arguments")
-		}
-
-		return Output{
-			Load: Param{imm1, code[1]},
-		}, 2, nil
-
-	case OpJumpIfTrue:
-		if len(code) < 3 {
-			return Halt{}, 0, fmt.Errorf("jump-if-true requires 2 arguments")
-		}
-
-		return JumpIfTrue{
-			Check:  Param{imm1, code[1]},
-			Target: Param{imm2, code[2]},
-		}, 3, nil
-
-	case OpJumpIfFalse:
-		if len(code) < 3 {
-			return Halt{}, 0, fmt.Errorf("jump-if-false requires 2 arguments")
-		}
-
-		return JumpIfFalse{
-			Check:  Param{imm1, code[1]},
-			Target: Param{imm2, code[2]},
-		}, 3, nil
-
-	case OpLessThan:
-		if len(code) < 4 {
-			return Halt{}, 0, fmt.Errorf("less-than requires 3 arguments")
-		}
-
-		return LessThan{
-			A:     Param{imm1, code[1]},
-			B:     Param{imm2, code[2]},
-			Store: Param{imm3, code[3]},
-		}, 4, nil
-
-	case OpEquals:
-		if len(code) < 4 {
-			return Halt{}, 0, fmt.Errorf("equls requires 3 arguments")
-		}
-
-		return Equals{
-			A:     Param{imm1, code[1]},
-			B:     Param{imm2, code[2]},
-			Store: Param{imm3, code[3]},
-		}, 4, nil
-
-	case OpHalt:
-		return Halt{}, 1, nil
-
-	default:
-		return Halt{}, 0, fmt.Errorf("unknown opcode %d", code[0])
+func Decode0(code Code) (op OpCode, advance int64, err error) {
+	if len(code) < 1 {
+		return OpHalt, 0, fmt.Errorf("requires opcode")
 	}
+
+	opcode, _, _, _ := ParseInstructionCode(code[0])
+	return opcode, 1, nil
+}
+
+func Decode1(code Code) (op OpCode, a1 Param, advance int64, err error) {
+	if len(code) < 2 {
+		return OpHalt, Param{}, 0, fmt.Errorf("requires 1 argument")
+	}
+
+	opcode, imm1, _, _ := ParseInstructionCode(code[0])
+	return opcode,
+		Param{imm1, code[1]},
+		2,
+		nil
+}
+
+func Decode2(code Code) (op OpCode, a1, a2 Param, advance int64, err error) {
+	if len(code) < 3 {
+		return OpHalt, Param{}, Param{}, 0, fmt.Errorf("requires 2 arguments")
+	}
+
+	opcode, imm1, imm2, _ := ParseInstructionCode(code[0])
+	return opcode,
+		Param{imm1, code[1]},
+		Param{imm2, code[2]},
+		3,
+		nil
+}
+
+func Decode3(code Code) (op OpCode, a1, a2, a3 Param, advance int64, err error) {
+	if len(code) < 4 {
+		return OpHalt, Param{}, Param{}, Param{}, 0, fmt.Errorf("requires 3 arguments")
+	}
+
+	opcode, imm1, imm2, imm3 := ParseInstructionCode(code[0])
+	return opcode,
+		Param{imm1, code[1]},
+		Param{imm2, code[2]},
+		Param{imm3, code[3]},
+		4,
+		nil
 }
