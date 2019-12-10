ac220587c2f3cb5b394e7c8729818316ad6ca7b7
Egon Elbre <egonelbre@gmail.com>
2019-12-10 14:44:42 +0200

    2019.08: part 2 failed

--- 2019/day08/main.go
+++ 2019/day08/main.go
@@ -16,13 +16,30 @@ func main() {
 	}
 
 	fmt.Println(leastDense.Count(1) * leastDense.Count(2))
+
+	composite := NewImage(leastDense.Width, leastDense.Height)
+	composite.Fill(Transparent)
+
+	for i := len(layers) - 1; i >= 0; i-- {
+		layers[i].DrawTo(composite)
+	}
+
+	composite.Print()
 }
 
 type Image struct {
-	Data   []byte
+	Data   []Color
 	Width  int64
 	Height int64
 }
+
+type Color = byte
+
+const (
+	Black       = Color(0)
+	White       = Color(1)
+	Transparent = Color(2)
+)
 
 func NewImage(width, height int64) *Image {
 	return &Image{
@@ -30,11 +47,42 @@ func NewImage(width, height int64) *Image {
 	}
 }
 
+func (image *Image) Fill(c Color) {
+	for i := range image.Data {
+		image.Data[i] = c
+	}
+}
+
+func (image *Image) DrawTo(target *Image) {
+	for i, src := range image.Data {
+		if src != Transparent {
+			target.Data[i] = src
+		}
+	}
+}
+
+func (image *Image) Print() {
+	for y := int64(0); y < image.Height; y++ {
+		for x := int64(0); x < image.Width; x++ {
+			var c = image.Data[image.Index(x, y)]
+			switch c {
+			case Black:
+				fmt.Print("#")
+			case White:
+				fmt.Print(" ")
+			case Transparent:
+				fmt.Print("?")
+			}
+		}
+		fmt.Println()
+	}
+}
+
 func (image *Image) Index(x, y int64) int64 {
 	return image.Height*y + x
 }
 
-func (image *Image) Count(b byte) int64 {
+func (image *Image) Count(b Color) int64 {
 	var count int64
 	for _, v := range image.Data {
 		if v == b {
