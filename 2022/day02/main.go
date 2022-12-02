e32f3d9277eed75b6a1c99a082178c7658a6c125
Egon Elbre <egonelbre@gmail.com>
2022-12-02 14:06:59 +0200

    2022.02: part 2

--- 2022/day02/main.go
+++ 2022/day02/main.go
@@ -24,53 +24,112 @@ func run() error {
 		return fmt.Errorf("failed to parse guide: %w", err)
 	}
 
-	fmt.Println(guide.Score())
+	fmt.Println(guide.Score(Basic))
+	fmt.Println(guide.Score(Advanced))
 
 	return nil
+}
+
+func Basic(_ Weapon, r Rule) Weapon {
+	switch r {
+	case RuleX:
+		return Rock
+	case RuleY:
+		return Paper
+	case RuleZ:
+		return Scissors
+	}
+	panic("invalid")
+}
+
+func Advanced(opponent Weapon, r Rule) Weapon {
+	switch r {
+	case RuleX:
+		return opponent.WinsAgainst()
+	case RuleY:
+		return opponent
+	case RuleZ:
+		return opponent.LosesAgainst()
+	}
+	panic("invalid")
 }
 
 type StrategyGuide struct {
 	Rounds []Round
 }
 
-func (guide *StrategyGuide) Score() int {
+type Strategy func(Weapon, Rule) Weapon
+
+func (guide *StrategyGuide) Score(strategy Strategy) int {
 	var total int
 	for _, round := range guide.Rounds {
-		total += round.Score()
+		total += round.Score(strategy)
 	}
 	return total
 }
 
 type Round struct {
 	Opponent Weapon
-	Response Weapon
+	Rule     Rule
 }
 
-func (r Round) Score() int {
-	if r.Opponent == r.Response {
-		return r.Response + 3
-	}
-
-	switch r {
-	case Round{Rock, Paper},
-		Round{Paper, Scissors},
-		Round{Scissors, Rock}:
-		return r.Response + 6
-	case Round{Paper, Rock},
-		Round{Scissors, Paper},
-		Round{Rock, Scissors}:
-		return r.Response + 0
-	}
-
-	panic("invalid")
+func (r Round) Score(strategy Strategy) int {
+	player := strategy(r.Opponent, r.Rule)
+	return score(r.Opponent, player)
 }
 
-type Weapon = int
+func score(opponent, player Weapon) int {
+	if opponent == player {
+		return player.Score() + 3
+	}
+
+	if player.WinsAgainst() == opponent {
+		return player.Score() + 6
+	}
+
+	return player.Score() + 0
+}
+
+type Weapon byte
 
 const (
 	Rock     Weapon = 1
 	Paper    Weapon = 2
 	Scissors Weapon = 3
+)
+
+func (weapon Weapon) LosesAgainst() Weapon {
+	switch weapon {
+	case Rock:
+		return Paper
+	case Paper:
+		return Scissors
+	case Scissors:
+		return Rock
+	}
+	panic("invalid")
+}
+
+func (weapon Weapon) WinsAgainst() Weapon {
+	switch weapon {
+	case Rock:
+		return Scissors
+	case Paper:
+		return Rock
+	case Scissors:
+		return Paper
+	}
+	panic("invalid")
+}
+
+func (weapon Weapon) Score() int { return int(weapon) }
+
+type Rule int
+
+const (
+	RuleX Rule = 1
+	RuleY Rule = 2
+	RuleZ Rule = 3
 )
 
 func ParseStrategyGuide(input string) (StrategyGuide, error) {
@@ -97,11 +156,11 @@ func ParseStrategyGuide(input string) (StrategyGuide, error) {
 
 		switch ans {
 		case "X":
-			round.Response = Rock
+			round.Rule = RuleX
 		case "Y":
-			round.Response = Paper
+			round.Rule = RuleY
 		case "Z":
-			round.Response = Scissors
+			round.Rule = RuleZ
 		default:
 			return guide, fmt.Errorf("invalid line #%d %q", lineno, line)
 		}
